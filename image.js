564b585e69c07df58f59a2c2a14ac04d96f00d1d
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2021-02-18 14:00:02 -0300

    Update image.js

--- image.js
+++ image.js
@@ -1,9 +1,12 @@
-<script>
-    var imgs = [
-        "https://i.imgur.com/or5foMa.jpg",
-        "https://i.imgur.com/g82jRNO.jpg",
-        "https://i.imgur.com/PU6tKcK.jpg",
-        "https://i.imgur.com/QbUzipX.jpg"
-        ];
-    document.write (imgs[1]);
-</script>
+window.onload = choosePic;
+
+var imgs = [
+    "https://i.imgur.com/or5foMa.jpg",
+    "https://i.imgur.com/g82jRNO.jpg",
+    "https://i.imgur.com/PU6tKcK.jpg",
+    "https://i.imgur.com/QbUzipX.jpg"
+];
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * imgs.length);
+     document.getElementById("myPicture").src = imgs[randomNum];
