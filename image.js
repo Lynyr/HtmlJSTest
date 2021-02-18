ae372a67431d3e5720d07fcade16fa13e62fc0ca
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2021-02-18 13:42:03 -0300

    Update image.js

--- image.js
+++ image.js
@@ -1,7 +1,9 @@
-var imgs = [
-    "https://i.imgur.com/or5foMa.jpg",
-    "https://i.imgur.com/g82jRNO.jpg",
-    "https://i.imgur.com/PU6tKcK.jpg",
-    "https://i.imgur.com/QbUzipX.jpg"
-    ];
-document.write (imgs[1]);
+<script>
+    var imgs = [
+        "https://i.imgur.com/or5foMa.jpg",
+        "https://i.imgur.com/g82jRNO.jpg",
+        "https://i.imgur.com/PU6tKcK.jpg",
+        "https://i.imgur.com/QbUzipX.jpg"
+        ];
+    document.write (imgs[1]);
+</script>
