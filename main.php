5a8f872ef28819fbd07282ec7040c828bae49671
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2021-02-18 12:43:58 -0300

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <html style="height: 100%;">
   <head>
     <meta name="viewport" content="width=device-width, minimum-scale=0.1">
-      <title>icon.php (300×300)</title>
+    <title>icon.php (300×300)</title>
   </head>
   <body style="margin: 0px; background: #0e0e0e; height: 100%">
     <img style="-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://atenaign.com/atena/icon.php">
