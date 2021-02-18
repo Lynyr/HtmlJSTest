94af1cc464d4f503f4f48dba048ad4ebc0edf689
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2021-02-18 13:30:41 -0300

    Update image.php

--- image.php
+++ image.php
@@ -1,11 +1,12 @@
 <html>
-  <?php
-    $array = [
-      "https://i.imgur.com/or5foMa.jpg",
-      "https://i.imgur.com/g82jRNO.jpg",
-      "https://i.imgur.com/PU6tKcK.jpg",
-      "https://i.imgur.com/QbUzipX.jpg"
-    ];
-    echo $array[rand(0, sizeof($array)-1)];
-  ?>
+  <img src="
+    <?php
+      $array = [
+        "https://i.imgur.com/or5foMa.jpg",
+        "https://i.imgur.com/g82jRNO.jpg",
+        "https://i.imgur.com/PU6tKcK.jpg",
+        "https://i.imgur.com/QbUzipX.jpg"
+      ];
+      echo $array[rand(0, sizeof($array)-1)];
+    ?>">
 </html>
