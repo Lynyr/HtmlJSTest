bfbee997a821bf82937b959636af83a93b73101b
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2021-02-18 13:34:52 -0300

    Update and rename image.php to image.js

---
+++ image.js
@@ -0,0 +1,7 @@
+var imgs = [
+    "https://i.imgur.com/or5foMa.jpg",
+    "https://i.imgur.com/g82jRNO.jpg",
+    "https://i.imgur.com/PU6tKcK.jpg",
+    "https://i.imgur.com/QbUzipX.jpg"
+    ];
+document.write (imgs[rand(0, sizeof($array)-1)]);
