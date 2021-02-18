412d87114e3f8eda37b0d03efc09b295b8699b64
Miyamoto <15330655+Lynyr@users.noreply.github.com>
2021-02-18 13:09:06 -0300

    Create image.php

---
+++ image.php
@@ -0,0 +1,9 @@
+<?php
+  $array = [
+    https://i.imgur.com/or5foMa.jpg,
+    https://i.imgur.com/g82jRNO.jpg,
+    https://i.imgur.com/PU6tKcK.jpg,
+    https://i.imgur.com/QbUzipX.jpg
+  ];
+  <img src = $array[rand(0,sizeof($array)-1];
+?>
